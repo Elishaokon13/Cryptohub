d0e95217822e06e58b93730cb4674ba7d81e9b54
Elisha Okon <eliscodavid@gmail.com>
2023-09-15 19:54:36 +0000

    init thirdweb

---
+++ cryptohub/const/yourDetails.ts
@@ -0,0 +1,11 @@
+// Replace this with your contract address
+export const contractAddress = "0x436492DBc2E30E56FaC8F2297BD1964833c0687d";
+
+// Replace this with your domain name.
+export const domainName = "thirdweb-example.com";
+
+// Minimum number of tokens required to access
+export const minimumBalance = 1;
+
+// Token ID for ERC-1155 tokens
+export const erc1155TokenId = 0;
---
+++ cryptohub/src/util/checkBalance.ts
@@ -0,0 +1,28 @@
+import { detectFeatures } from "@thirdweb-dev/sdk";
+import {
+  contractAddress,
+  erc1155TokenId,
+  minimumBalance,
+} from "const/yourDetails";
+
+export default async function checkBalance(sdk, address) {
+  const contract = await sdk.getContract(
+    contractAddress // replace this with your contract address
+  );
+
+  let balance;
+
+  const features = detectFeatures(contract.abi);
+
+  if (features?.ERC1155?.enabled) {
+    balance = await contract.erc1155.balanceOf(address, erc1155TokenId);
+  } else if (features?.ERC721?.enabled) {
+    balance = await contract.erc721.balanceOf(address);
+  } else if (features?.ERC20?.enabled) {
+    balance = (await contract.erc20.balanceOf(address)).value;
+    return balance.gte((minimumBalance * 1e18).toString());
+  }
+
+  // gte = greater than or equal to
+  return balance.gte(minimumBalance);
+}
